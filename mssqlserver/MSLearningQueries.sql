8a3a509f4a10bdc4c8156a7f4197efd547922f7c
Ygor Nascimento <ygor.nascimento@outlook.com>
2026-08-15 17:14:03 -0300

    Exemplos de Queries no MSSQL

--- mssqlserver/MSLearningQueries.sql
+++ mssqlserver/MSLearningQueries.sql
@@ -37,3 +37,47 @@ SELECT Name, TRY_CAST(Size AS Integer) AS NumericSize
 FROM SalesLT.Product;
 
 -- Handle NULL Values
+SELECT Name, ISNULL(TRY_CAST(Size AS Integer), 0) AS NumericSize
+FROM SalesLT.Product;
+
+SELECT ProductNumber, ISNULL(Color, '') + ', ' + ISNULL(Size, '') AS ProductDetails
+FROM SalesLT.Product;
+
+SELECT Name, NULLIF(Color, 'Multi') AS SingleColor
+FROM SalesLT.Product;
+
+SELECT Name, Color AS SingleColor
+FROM SalesLT.Product;
+
+SELECT Name, COALESCE(SellEndDate, SellStartDate) AS StatusLastUpdated
+FROM SalesLT.Product;
+
+--searched CASE
+SELECT Name,
+	CASE
+		WHEN SellEndDate IS NULL THEN 'Currently for sale'
+		ELSE 'No longer available'
+	END AS SalesStatus
+FROM SalesLT.Product;
+
+--simple CASE
+SELECT Name,
+	CASE Size
+		WHEN 'S' THEN 'Small'
+		WHEN 'M' THEN 'Medium'
+		WHEN 'L' THEN 'Large'
+		WHEN 'XL' THEN 'Extra Large'
+		ELSE ISNULL(Size, 'n/a')
+	END AS ProductSize
+FROM SalesLT.Product;
+
+SELECT Name,
+	CASE Size
+		WHEN 'S' THEN 'Small'
+		WHEN 'M' THEN 'Medium'
+		WHEN 'L' THEN 'Large'
+		WHEN 'XL' THEN 'Extra Large'
+		--ELSE ISNULL(Size, 'n/a')
+	END AS ProductSize
+FROM SalesLT.Product;
+
